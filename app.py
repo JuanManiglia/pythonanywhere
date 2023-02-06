ba28ee462a2f96225aba80b2be882c0347b815a4
JuanManiglia <jamr1989@gmail.com>
2023-02-06 21:37:02 +0100

    con la hora

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ def git_update():
 @app.route('/', methods=['GET'])
 def index():
 
-    return 'mi primera Web cutre.... Soy Juan Maniglia.... al fin'
+    return 'mi primera Web cutre.... Soy Juan Maniglia.... al fin son las 21.36'
 
 @app.route('/v2/', methods=['GET'])
 def otra():
