148c6a7f3fed526910c0b9f08fe316a8e20afcf9
JuanManiglia <jamr1989@gmail.com>
2023-02-06 21:20:50 +0100

    argergerg

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ def git_update():
 @app.route('/', methods=['GET'])
 def index():
 
-    return 'mi primera Web cutre.... Soy Juan Maniglia lolo'
+    return 'mi primera Web cutre.... Soy Juan Maniglia jksadfkljsdbliwaebfñlikwebfñb'
 
 @app.route('/v2/', methods=['GET'])
 def otra():
